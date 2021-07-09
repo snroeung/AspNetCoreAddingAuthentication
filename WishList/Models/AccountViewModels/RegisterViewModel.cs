e24c3dc2a37b87562c714de6916e69befb2b2b8f
Sarenna Roeung <sroeung@emoneyadvisor.com>
2021-07-09 15:59:28 -0400

    Completed module

--- WishList/Models/AccountViewModels/RegisterViewModel.cs
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -9,6 +9,7 @@ namespace WishList.Models.AccountViewModels
     public class RegisterViewModel
     {
         [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         [Required]
         [StringLength(100, MinimumLength = 8)]
